bd63d56ec7d9142526a75e6e3d702ffc1d2b0e5c
Andres <andres039039@Hotmail.com>
2021-10-03 21:17:14 +0000

    previous files

---
+++ loopyLighthouse.js
@@ -0,0 +1,8 @@
+for (let i = 100; i < 201; i++) {
+  console.log(i % 3 === 0 ? (i % 4 === 0 ? 'LoopyLighthouse' : 'Loopy') : i % 4 === 0 ? 'Lighthouse' : i)
+// for (let j = 0; j < array.length; j++) {
+//   const element = array[j];
+  
+// }
+  
+}
